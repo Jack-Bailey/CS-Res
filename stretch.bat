a5dab61674e99e065c07a49c233b3a726a620d21
Jack Bailey <contact@jackbailey.uk>
2021-09-16 17:58:05 +0100

    Delete stretch.bat

--- stretch.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-"qres\qres.exe" x=1152 y=864 f=144
-"C:\Program Files (x86)\Cursor Lock\CursorLock.exe"  /O:"C:\Program Files (x86)\Steam\steam.exe" /P:"-applaunch 730" /A:"C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo.exe"
-
-"qres\qres.exe" x=1920 y=1080 f=144
